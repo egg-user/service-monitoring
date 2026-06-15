9b39a92943a035ba88fe5ae3a23a1cb21d62df9e
egg-user <egakuswardana@gmail.com>
2026-06-15 12:04:18 +0700

    Implement SSH service status check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import paramiko
 
 base_path = os.getcwd()
 servers_path = os.path.join(base_path, "servers.txt")
@@ -20,7 +21,28 @@ def read_services_path():
             services.append(service.strip())
     return services
 
+def check_service(servers_ip, services):
+    try:
+        services_status = []
+        for ip in servers_ip:
+            for service in services:
+                username = "egg"
+                host = ip
+                password = "change"
+                client = paramiko.client.SSHClient()
+                client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                client.connect(host, username=username, password=password)
+                stdin, stdout, stderr = client.exec_command(f"systemctl is-active {service}")
+                service_server = stdout.read().decode().strip()
+                services_status.append((
+                    ip, service, service_server
+                ))
+        print(services_status)
+    
+    except:
+        print("Host Unreachable")
+
         
 servers_ip = read_servers_path()
 services = read_services_path()
-print(services)
+check_service(servers_ip, services)
